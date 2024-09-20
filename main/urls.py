d7a33f912a177c8caa8cc1de93136bd2a088097b
Kumita Akira <akira_kumita@demia.co.jp>
2024-09-20 17:08:25 +0900

    5.カート機能を実装する1

--- main/urls.py
+++ main/urls.py
@@ -7,8 +7,9 @@ urlpatterns = [
     path("login/", views.Login.as_view(), name="login"),
     path("logout/", views.Logout.as_view(), name="logout"),
     path("home/", views.ProductList.as_view(), name="home"),
-    # path(
-    #     "product_detail/<int:pk>", views.ProductDetail.as_view(), name="product_detail"
-    # ),
+    path(
+        "product_detail/<int:pk>", views.ProductDetail.as_view(), name="product_detail"
+    ),
     path("account/", views.AccountView.as_view(), name="account"),
+    path("cart/", views.Cart.as_view(), name="cart"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,11 @@
 from typing import Any
+from urllib import request
 
 from django.contrib.auth import login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.db.models.query import QuerySet
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
@@ -73,3 +75,63 @@ class ProductDetail(DetailView):
         context = super().get_context_data(**kwargs)
         context["form"] = ProductNumForm()
         return context
+
+    def post(self, request, *args, **kwargs):
+        pk = self.kwargs["pk"]
+        num = self.request.POST.get("num")
+
+        if "cart" in self.request.session:
+            # cartに入っている商品
+            cart = self.request.session["cart"]
+            # 入っている商品ごとの個数(辞書型)
+            num_dict = self.request.session["num_dict"]
+
+            if str(pk) in num_dict:
+                num_dict[str(pk)] += int(num)
+            else:
+                cart.append(pk)
+                num_dict[str(pk)] = int(num)
+        else:
+            self.request.session["cart"] = [pk]
+            self.request.session["num_dict"] = {str(pk): int(num)}
+        return redirect("home")
+
+
+class Cart(LoginRequiredMixin, ListView):
+    paginate_by = 5
+    template_name = "main/cart.html"
+
+    # get_querysetの後に実行される
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        if "cart" in self.request.session:
+            num_dict = self.request.session["num_dict"]
+            context["total_price"] = sum(
+                self.product_net(product, num_dict) for product in self.products
+            )
+        return context
+
+    def get_queryset(self):
+        if "cart" in self.request.session:
+            self.products = Product.objects.filter(
+                pk__in=self.request.session["cart"]
+            ).order_by("pk")
+            return self.products
+        else:
+            # 空のクエリセットを返すためのメソッド
+            return Product.objects.none()
+
+    def product_net(self, product, num_dict):
+        """一つの商品に対して小計を計算する関数"""
+        # num_dictからproductのidを利用して数を取得
+        return product.price * int(num_dict[str(product.pk)])
+
+    def post(self, *args, **kwargs):
+        pk = self.request.POST.get("delete_pk")
+        cart = self.request.session["cart"]
+        num_dict = self.request.session["num_dict"]
+        cart.remove(int(pk))
+        num_dict.pop(pk)
+        self.request.session["cart"] = cart
+        self.request.session["num_dict"] = num_dict
+        return redirect("cart")
